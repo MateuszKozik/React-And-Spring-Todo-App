491f1ea30bfaeadf1734282bc0da14d454d67754
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-11 22:28:50 +0100

    Create test component

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import TestComponent from './components/test/TestComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Hello world
+        <TestComponent/>
       </header>
     </div>
   );
---
+++ frontend/todo-app/src/components/test/TestComponent.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+export default function TestComponent() {
+    return (
+      <div className="testComponent">
+        Test
+      </div>
+    );
+  }
+  
