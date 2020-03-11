86930407b425e00cf81f520f7674c523e94e6730
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-11 22:33:52 +0100

    Move export to the EOF

--- frontend/todo-app/src/components/test/TestComponent.jsx
+++ frontend/todo-app/src/components/test/TestComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function TestComponent() {
+function TestComponent() {
     return (
       <div className="testComponent">
         Test
@@ -8,3 +8,4 @@ export default function TestComponent() {
     );
   }
   
+export default TestComponent;
