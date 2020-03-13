b223268ddf3df97a2c40c5280e25a844c066330a
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-13 18:01:34 +0100

    Improve footer displayed

--- frontend/todo-app/src/components/todo/TodoApp.js
+++ frontend/todo-app/src/components/todo/TodoApp.js
@@ -46,9 +46,9 @@ class HeaderComponent extends Component {
 class FooterComponent extends Component {
     render() {
         return (
-            <div>
-                <hr/> Footer
-            </div>
+           <footer className="footer">
+                <span className="text-muted">All Rights Reserved 2020 @MateuszKozik</span>
+           </footer>
         )
     }
 }
