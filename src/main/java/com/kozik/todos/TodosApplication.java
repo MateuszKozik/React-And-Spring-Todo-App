0e2bc3e27598315e2cca77f99bd34e2a7a9c74ca
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-14 19:34:07 +0100

    Disable Spring Security

--- src/main/java/com/kozik/todos/TodosApplication.java
+++ src/main/java/com/kozik/todos/TodosApplication.java
@@ -2,8 +2,9 @@ package com.kozik.todos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class})
 public class TodosApplication {
 
 	public static void main(String[] args) {
