e63cee0af7edd34e1a876ef5ed985899c4b1eb41
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-15 17:16:01 +0100

    Create delete mapping

--- src/main/java/com/kozik/todos/controllers/TodoController.java
+++ src/main/java/com/kozik/todos/controllers/TodoController.java
@@ -6,8 +6,11 @@ import com.kozik.todos.entities.Todo;
 import com.kozik.todos.services.TodoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +23,14 @@ public class TodoController {
     @GetMapping("/users/{user_name}/todos")
     public List<Todo> getAllTodos(String username){
         return todoService.findAll();
+    }
+
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<?> deleteTodo(@PathVariable String username, @PathVariable Long id){
+        Todo todo = todoService.deleteById(id);
+        if(todo!=null){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
