2b4a6193cc537bc55a80fce171e0535bda518466
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-15 12:26:10 +0100

    Create mapping to get all Todos

---
+++ src/main/java/com/kozik/todos/controllers/TodoController.java
@@ -0,0 +1,22 @@
+package com.kozik.todos.controllers;
+
+import java.util.List;
+
+import com.kozik.todos.entities.Todo;
+import com.kozik.todos.services.TodoService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @GetMapping("/users/{user_name}/todos")
+    public List<Todo> getAllTodos(String username){
+        return todoService.findAll();
+    }
+}
