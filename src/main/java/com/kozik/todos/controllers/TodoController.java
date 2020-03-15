67e1b04c5bb8b0291ad4660930e52136aa0550a5
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-15 13:34:45 +0100

    Add CrossOrigin annotation

--- src/main/java/com/kozik/todos/controllers/TodoController.java
+++ src/main/java/com/kozik/todos/controllers/TodoController.java
@@ -6,10 +6,12 @@ import com.kozik.todos.entities.Todo;
 import com.kozik.todos.services.TodoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class TodoController {
 
     @Autowired
