d7610db8f6110f93c0d0f9cb43df43734e7e201d
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-15 12:11:10 +0100

    Create Todo entity

---
+++ src/main/java/com/kozik/todos/entities/Todo.java
@@ -0,0 +1,58 @@
+package com.kozik.todos.entities;
+
+import java.util.Date;
+
+public class Todo {
+    private Long id;
+    private String username;
+    private String description;
+    private Date targetDate;
+    private Boolean isDone;
+
+    public Todo() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getTargetDate() {
+        return this.targetDate;
+    }
+
+    public void setTargetDate(Date targetDate) {
+        this.targetDate = targetDate;
+    }
+
+    public Boolean isIsDone() {
+        return this.isDone;
+    }
+
+    public Boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setIsDone(Boolean isDone) {
+        this.isDone = isDone;
+    }
+}
