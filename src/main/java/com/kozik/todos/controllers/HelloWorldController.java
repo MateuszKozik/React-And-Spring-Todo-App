37f065e4a86b6ecab130e941915c0022a682188f
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-15 10:20:21 +0100

    Add CrossOrigin annotation

--- src/main/java/com/kozik/todos/controllers/HelloWorldController.java
+++ src/main/java/com/kozik/todos/controllers/HelloWorldController.java
@@ -2,10 +2,12 @@ package com.kozik.todos.controllers;
 
 import com.kozik.todos.utilities.Message;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="http://localhost:3000")
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
