266cc329dbf7a49761f3abdaa622f2e436e398dd
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-03-15 13:40:44 +0100

    Display data from response in the table

--- frontend/todo-app/src/components/todo/ListTodosComponent.js
+++ frontend/todo-app/src/components/todo/ListTodosComponent.js
@@ -14,7 +14,9 @@ class ListTodosComponent extends Component {
     componentDidMount() {
         let username = AuthenticationService.getLoggedInUserName
         TodoDataService.retieveAllTodos(username)
-            .then( response => console.log(response) )
+            .then( response => {
+                this.setState({todos : response.data})
+            } )
     }
 
     render(){
@@ -36,7 +38,7 @@ class ListTodosComponent extends Component {
                                 todo =>
                                 <tr key={todo.id}>
                                     <td>{todo.description}</td>
-                                    <td>{todo.done.toString()}</td>
+                                    <td>{todo.isDone.toString()}</td>
                                     <td>{todo.targetDate.toString()}</td>
                                 </tr>
                             )
